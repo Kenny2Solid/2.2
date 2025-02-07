53e1e84b560394adbe71a947b735ec98d64f8130
Kendrick Torrance <torraken000@warren.k12.in.us>
2025-02-07 08:10:46 -0500

    2nd

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Happy developing ✨')
+
+
+let book = {
+    title: "diary of a wimpy kid",
+    author: "Jeff Kinney",
+    year: "2007",
+};
+
+
+book.getSummary = function() {
+    return `${this.title} by ${this.author}, published in ${this.year}.`;
+};
+
+
+book.genre = "Fiction";
+
+
+delete book.year;
+
+
+console.log(book);
+console.log(book.getSummary());
+
+
